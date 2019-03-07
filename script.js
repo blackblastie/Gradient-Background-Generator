72e6930a4c6ab4b658f1ef05697bdb16ad2d6f95
Alex McGlothlin <v35711@gmail.com>
2019-03-07 12:03:17 -0600

    Initial Upload

---
+++ script.js
@@ -0,0 +1,32 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("Gradient");
+var button = document.querySelector("button")
+
+setGradient();
+
+function setGradient() {
+		body.style.background = 
+		"linear-gradient(to right, "
+		 + color1.value 
+		 + ", " 
+		 + color2.value
+		 + ")";
+
+		 css.textContent = body.style.background + ";";
+}
+
+function randomColors() {
+	var randomColor1 = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+	var randomColor2 = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+	color1.value = randomColor1;
+	color2.value = randomColor2;
+	setGradient();
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+button.addEventListener("click", randomColors);
